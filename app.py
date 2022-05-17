fb1c971a3108e796b3deff2e13afb08b4bf8bf55
Maheshvaran <75715855+MaheshvaranS@users.noreply.github.com>
2022-05-17 15:28:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import render_template
 # creates a Flask application, named app
 app = Flask(__name__, static_url_path='/static')
 
-# a route to display our html page gotten from [react-chat-widget](https://github.com/mrbot-ai/rasa-webchat)
+# a route to display our html page
 @app.route("/")
 def index():  
     return render_template('index.html')
