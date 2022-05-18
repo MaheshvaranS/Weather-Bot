0197238fb7d93a4acb8f59fa78cf6e77f3ec6028
Maheshvaran <75715855+MaheshvaranS@users.noreply.github.com>
2022-05-18 12:23:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask
 from flask import render_template
 
 # creates a Flask application, named app
-app = Flask(__name__, static_url_path='/app/static')
+app = Flask(__name__, static_url_path='static')
 
 
 # a route to display our html page
 @app.route("/")
 def index():
-    return render_template('/app/templates/index.html')
+    return render_template('index.html')
 
 
 # run the application
