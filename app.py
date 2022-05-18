79b0886dd0792649a34ae10cc99b9da06eba262e
Maheshvaran <75715855+MaheshvaranS@users.noreply.github.com>
2022-05-18 12:24:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 
 # creates a Flask application, named app
+app=Flask(__name__,template_folder='templates')
 app = Flask(__name__, static_url_path='static')
 
 
